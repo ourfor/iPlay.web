cb1a26f99699f7cc5e41ef501a6cdfb3219b338d
ourfor <ourfor@qq.com>
2023-08-19 15:16:16 +0800

    🎨 adapt mobile device

--- src/page/album/index.tsx
+++ src/page/album/index.tsx
@@ -22,7 +22,7 @@ export default function Page() {
     return (
         <div className={style["page"]}>
             <Background src={imageUrl(id, album?.Etag ?? "")} />
-            <h3>{album?.Name}</h3>
+            <h3 className={style.title}>{album?.Name}</h3>
             <div className={style["content"]}>
                 {data && data.map(media => <MediaCard key={media.Id} {...media} />)}
             </div>
