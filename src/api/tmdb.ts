175969e7285b78dfa85abd4bb5d26f611f3c5076
ourfor <ourfor@qq.com>
2024-03-12 18:19:31 +0800

    ✨ update search page

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -56,22 +56,8 @@ export namespace TMDB {
     export interface SearchResponse {
         page: number,
         results: Partial<MovieDetail>[],
-        // results: {
-        //     adult: boolean,
-        //     backdrop_path: string,
-        //     genre_ids: number[],
-        //     id: number,
-        //     original_language: string,
-        //     original_title: string,
-        //     overview: string,
-        //     popularity: number,
-        //     poster_path: string,
-        //     release_date: string,
-        //     title: string,
-        //     video: boolean,
-        //     vote_average: number,
-        //     vote_count: number
-        // }[]
+        total_pages: number,
+        total_results: number
     }
 
     export async function searchMovie(query: string, page: number = 1): Promise<SearchResponse | null>{
---
+++ src/components/search/Search.tsx
@@ -0,0 +1,28 @@
+import { Button, TextField } from "@radix-ui/themes";
+import style from "./Search.module.scss"
+import { useState } from "react";
+import React from "react";
+
+interface SearchProps {
+    initValue?: string,
+    className?: string,
+    onValueChange?: (value: string) => void
+}
+
+export const Search: React.FC<SearchProps> = (props) => {
+    const { onValueChange, initValue = "" } = props
+    const [value, setValue] = useState(initValue)
+    return (
+        <div className={[style.root, props.className].join(" ")}>
+            <TextField.Input value={value}
+                required 
+                placeholder="搜索关键字" 
+                onChange={e => setValue(e.target.value)}
+                type="text" />
+            <Button size={"2"} 
+                onClick={() => onValueChange?.(value)}>
+                搜索
+            </Button>
+        </div>
+    )
+}
