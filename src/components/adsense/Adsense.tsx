85f6ea06743c363e314d2ac1b4042bff950c7507
ourfor <ourfor@qq.com>
2024-03-12 19:52:22 +0800

    🚨 update adsense component

--- src/components/adsense/Adsense.tsx
+++ src/components/adsense/Adsense.tsx
@@ -4,6 +4,10 @@ import { useEffect } from "react"
 
 export function Adsense() {
     if (!ENV.adsense.id || !ENV.adsense.slot) return null
+    return <AdsenseFixedArea />
+}
+
+export function AdsenseFixedArea() {
 
     const src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ENV.adsense.id}`
     const onLoad = () => {
@@ -19,6 +23,7 @@ export function Adsense() {
         document.head.appendChild(script)
         logger.info("load adsense")
     }, [])
+    
     return (
         <div className="ads-holder">
             <ins className="adsbygoogle"
