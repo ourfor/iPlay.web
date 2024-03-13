9e0e5f4f05a2edfdde0830600b7bf84eb3831897
ourfor <ourfor@qq.com>
2024-03-13 09:30:44 +0800

    💫 add explosion animation

--- src/components/firework/fireworks.js
+++ src/components/firework/fireworks.js
@@ -1,144 +1,154 @@
-// import anime from 'animejs';
-import anime from 'animejs';
+import anime from "animejs";
 
 /* eslint-disable */
-const colors = ["#FF1461", "#18FF92", "#5A87FF", "#FBF38C"];
-
-export function init(dom) {
-    let canvasEl;
-    let ctx,
-        numberOfParticules = 30,
-        pointerX = 0,
-        pointerY = 0,
-        tap = "mousedown",
-        setCanvasSize = debounce(function () {
-            canvasEl.width = 2 * window.innerWidth,
-                canvasEl.height = 2 * window.innerHeight,
-                canvasEl.style.width = window.innerWidth + "px",
-                canvasEl.style.height = window.innerHeight + "px",
-                canvasEl.getContext("2d").scale(2, 2)
-        },
-            500),
-        render = anime({
-            duration: 1 / 0,
-            update: function () {
-                ctx.clearRect(0, 0, canvasEl.width, canvasEl.height)
-            }
-        });
-
-    function updateCoords(e) {
-        pointerX = (e.clientX || e.touches[0].clientX) - canvasEl.getBoundingClientRect().left,
-            pointerY = e.clientY || e.touches[0].clientY - canvasEl.getBoundingClientRect().top
+class Circle {
+    constructor({ origin, speed, color, angle, context }) {
+        this.origin = origin
+        this.position = { ...this.origin }
+        this.color = color
+        this.speed = speed
+        this.angle = angle
+        this.context = context
+        this.renderCount = 0
     }
 
-    function animateParticules(e, t) {
-        for (var a = createCircle(e, t), n = [], i = 0; i < numberOfParticules; i++) {
-            n.push(createParticule(e, t))
-        }
-        anime.timeline().add({
-            targets: n,
-            x: function (e) {
-                return e.endPos.x
-            },
-            y: function (e) {
-                return e.endPos.y
-            },
-            radius: 0.1,
-            duration: anime.random(1200, 1800),
-            easing: "easeOutExpo",
-            update: renderParticule
-        }).add({
-            targets: a,
-            radius: anime.random(80, 160),
-            lineWidth: 0,
-            alpha: {
-                value: 0,
-                easing: "linear",
-                duration: anime.random(600, 800)
-            },
-            duration: anime.random(1200, 1800),
-            easing: "easeOutExpo",
-            update: renderParticule,
-            offset: 0
-        })
+    draw() {
+        this.context.fillStyle = this.color
+        this.context.beginPath()
+        this.context.arc(this.position.x, this.position.y, 2, 0, Math.PI * 2)
+        this.context.fill()
     }
 
-    function setParticuleDirection(e) {
-        var t = anime.random(0, 360) * Math.PI / 180,
-            a = anime.random(50, 180),
-            n = [-1, 1][anime.random(0, 1)] * a;
-        return {
-            x: e.x + n * Math.cos(t),
-            y: e.y + n * Math.sin(t)
-        }
-    }
-    
-    function createParticule(e, t) {
-        var a = {};
-        return a.x = e,
-            a.y = t,
-            a.color = colors[anime.random(0, colors.length - 1)],
-            a.radius = anime.random(16, 32),
-            a.endPos = setParticuleDirection(a),
-            a.draw = function () {
-                ctx.beginPath(),
-                    ctx.arc(a.x, a.y, a.radius, 0, 2 * Math.PI, !0),
-                    ctx.fillStyle = a.color,
-                    ctx.fill()
-            },
-            a
-    }
-    
-    function createCircle(e, t) {
-        var a = {};
-        return a.x = e,
-            a.y = t,
-            a.color = "#F00",
-            a.radius = 0.1,
-            a.alpha = 0.5,
-            a.lineWidth = 6,
-            a.draw = function () {
-                ctx.globalAlpha = a.alpha,
-                    ctx.beginPath(),
-                    ctx.arc(a.x, a.y, a.radius, 0, 2 * Math.PI, !0),
-                    ctx.lineWidth = a.lineWidth,
-                    ctx.strokeStyle = a.color,
-                    ctx.stroke(),
-                    ctx.globalAlpha = 1
-            },
-            a
-    }
-    
-    function renderParticule(e) {
-        for (var t = 0; t < e.animatables.length; t++) {
-            e.animatables[t].target.draw()
-        }
-    }
-    
-    
-    function debounce(e, t) {
-        var a;
-        return function () {
-            var n = this,
-                i = arguments;
-            clearTimeout(a),
-                a = setTimeout(function () {
-                    e.apply(n, i)
-                },
-                    t)
-        }
-    }
-
-    
-    if (canvasEl = dom) {
-        ctx = canvasEl.getContext("2d")
-        document.addEventListener(tap,
-            function (e) {
-                "sidebar" !== e.target.id && "poker" !== e.target.className && "toggle-sidebar" !== e.target.id && "A" !== e.target.nodeName && "IMG" !== e.target.nodeName && (render.play(), updateCoords(e), animateParticules(pointerX, pointerY))
-            },
-            !1),
-            setCanvasSize(),
-            window.addEventListener("resize", setCanvasSize, !1)
+    move() {
+        this.position.x = (Math.sin(this.angle) * this.speed) + this.position.x
+        this.position.y = (Math.cos(this.angle) * this.speed) + this.position.y + (this.renderCount * 0.3)
+        this.renderCount++
     }
 }
 
+class Boom {
+    constructor({ origin, context, circleCount = 16, area }) {
+        this.origin = origin
+        this.context = context
+        this.circleCount = circleCount
+        this.area = area
+        this.stop = false
+        this.circles = []
+    }
+
+    randomArray(range) {
+        const length = range.length
+        const randomIndex = Math.floor(length * Math.random())
+        return range[randomIndex]
+    }
+
+    randomColor() {
+        const range = ['8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+        return '#' + this.randomArray(range) + this.randomArray(range) + this.randomArray(range) + this.randomArray(range) + this.randomArray(range) + this.randomArray(range)
+    }
+
+    randomRange(start, end) {
+        return (end - start) * Math.random() + start
+    }
+
+    init() {
+        for (let i = 0; i < this.circleCount; i++) {
+            const circle = new Circle({
+                context: this.context,
+                origin: this.origin,
+                color: this.randomColor(),
+                angle: this.randomRange(Math.PI - 1, Math.PI + 1),
+                speed: this.randomRange(1, 6)
+            })
+            this.circles.push(circle)
+        }
+    }
+
+    move() {
+        this.circles.forEach((circle, index) => {
+            if (circle.position.x > this.area.width || circle.position.y > this.area.height) {
+                return this.circles.splice(index, 1)
+            }
+            circle.move()
+        })
+        if (this.circles.length == 0) {
+            this.stop = true
+        }
+    }
+
+    draw() {
+        this.circles.forEach(circle => circle.draw())
+    }
+}
+
+export class CursorSpecialEffects {
+    constructor() {
+        this.computerCanvas = document.createElement('canvas')
+        this.renderCanvas = document.createElement('canvas')
+
+        this.computerContext = this.computerCanvas.getContext('2d')
+        this.renderContext = this.renderCanvas.getContext('2d')
+
+        this.globalWidth = window.innerWidth
+        this.globalHeight = window.innerHeight
+
+        this.booms = []
+        this.running = false
+    }
+
+    handleMouseDown(e) {
+        const boom = new Boom({
+            origin: { x: e.clientX, y: e.clientY },
+            context: this.computerContext,
+            area: {
+                width: this.globalWidth,
+                height: this.globalHeight
+            }
+        })
+        boom.init()
+        this.booms.push(boom)
+        this.running || this.run()
+    }
+
+    handlePageHide() {
+        this.booms = []
+        this.running = false
+    }
+
+    init() {
+        const style = this.renderCanvas.style
+        style.position = 'fixed'
+        style.top = style.left = 0
+        style.zIndex = '999999999999999999999999999999999999999999'
+        style.pointerEvents = 'none'
+
+        style.width = this.renderCanvas.width = this.computerCanvas.width = this.globalWidth
+        style.height = this.renderCanvas.height = this.computerCanvas.height = this.globalHeight
+
+        document.body.append(this.renderCanvas)
+
+        window.addEventListener('mousedown', this.handleMouseDown.bind(this))
+        window.addEventListener('pagehide', this.handlePageHide.bind(this))
+    }
+
+    run() {
+        this.running = true
+        if (this.booms.length == 0) {
+            return this.running = false
+        }
+
+        requestAnimationFrame(this.run.bind(this))
+
+        this.computerContext.clearRect(0, 0, this.globalWidth, this.globalHeight)
+        this.renderContext.clearRect(0, 0, this.globalWidth, this.globalHeight)
+
+        this.booms.forEach((boom, index) => {
+            if (boom.stop) {
+                return this.booms.splice(index, 1)
+            }
+            boom.move()
+            boom.draw()
+        })
+        this.renderContext.drawImage(this.computerCanvas, 0, 0, this.globalWidth, this.globalHeight)
+    }
+}
