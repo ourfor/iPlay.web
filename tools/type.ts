cbc8fdf8a804a3156521dc9b715b8947b687500f
ourfor <ourfor@qq.com>
2024-03-15 15:07:26 +0800

    ✨ add tv show

--- tools/type.ts
+++ tools/type.ts
@@ -121,3 +121,66 @@ export interface SpokenLanguage {
     iso_639_1: string;
     name: string;
 }
+
+export interface Season {
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string;
+    season_number: number;
+}
+
+export interface Episode {
+    air_date: string;
+    episode_number: number;
+    id: number;
+    name: string;
+    overview: string;
+    production_code: string;
+    season_number: number;
+    still_path: string;
+    vote_average: number;
+    vote_count: number;
+}
+
+export interface Network {
+    name: string;
+    id: number;
+    logo_path: string;
+    origin_country: string;
+}
+
+export interface Series {
+    adult: boolean;
+    backdrop_path: string;
+    created_by: Person[];
+    episode_run_time: number[];
+    first_air_date: string;
+    genres: Genre[];
+    homepage: string;
+    id: number;
+    in_production: boolean;
+    languages: string[];
+    last_air_date: string;
+    last_episode_to_air: Episode;
+    name: string;
+    next_episode_to_air: Episode;
+    networks: Network[];
+    number_of_episodes: number;
+    number_of_seasons: number;
+    origin_country: string[];
+    original_language: string;
+    original_name: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    production_companies: ProductionCompany[];
+    seasons: Season[];
+    tagline: string;
+    status: string;
+    type: string;
+    vote_average: number;
+    vote_count: number;
+}
